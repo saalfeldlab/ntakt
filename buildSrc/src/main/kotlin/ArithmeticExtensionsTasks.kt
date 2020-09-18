8bf710ece180c0064b4e0862ef05aabc78f01a2b
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2020-09-17 22:38:31 -0400

    Add appveyor config (#11)
    * Start work on appveyor
    
    * Add appveyor badge
    
    * Debug windows build
    
    * Reduce logging output in windows build
    
    * Add --stacktrace option
    
    * Add more debugging stuff
    
    * Add more debugging stuff
    
    * Make logging more verbose
    
    * Add logging to correct task
    
    * Add more checks before build
    
    * Add more logging
    
    * Add more logging
    
    * Add more logging
    
    * Remove some debugging statements
    
    * Remove some debugging statements
    
    * Try building without removing file first
    
    * Try without creating directory
    
    * Re-add directory generation and add comment
    
    * Re-enable linux nad macos builds

--- buildSrc/src/main/kotlin/ArithmeticExtensionsTasks.kt
+++ buildSrc/src/main/kotlin/ArithmeticExtensionsTasks.kt
@@ -6,6 +6,9 @@ open class GenerateArithmeticExtensionsTask : ExtensionsTask("Arithmetic") {
     fun runTask() {
         for ((`as`, name) in typeFileMapping) {
             println("generating arithmetic extensions for $`as` ($name)")
+            // TODO for some reason, need to create parent directory to build successfully on Windows@appveyor
+            // TODO strangely, this does not affect any of the other extension tasks
+            Files.createDirectories(name.second.parentFile.toPath())
             Files.write(name.second.toPath(), generateArithmeticExtensions(`as`, name.first).withHeader.toByteArray())
         }
     }
