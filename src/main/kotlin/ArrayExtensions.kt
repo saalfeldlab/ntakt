a4959176694ad00341e6b28ab06f3d8e2daf9552
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2021-04-11 20:55:52 -0400

    Add convenience functions to create accesses from arrays

--- src/main/kotlin/ArrayExtensions.kt
+++ src/main/kotlin/ArrayExtensions.kt
@@ -25,6 +25,8 @@
  */
 package org.ntakt
 
+import net.imglib2.img.basictypeaccess.volatiles.array.*
+
 val ByteArray.shorts get() = ShortArray(size) { this[it].toShort() }
 
 val ByteArray.ints get() = IntArray(size) { this[it].toInt() }
@@ -44,3 +46,13 @@ val ShortArray.doubles get() = DoubleArray(size) { this[it].toDouble() }
 val IntArray.doubles get() = DoubleArray(size) { this[it].toDouble() }
 val LongArray.doubles get() = DoubleArray(size) { this[it].toDouble() }
 val FloatArray.doubles get() = DoubleArray(size) { this[it].toDouble() }
+
+// accesses
+val BooleanArray.access get() = VolatileBooleanArray(this, true)
+val CharArray.access get() = VolatileCharArray(this, true)
+val ByteArray.access get() = VolatileByteArray(this, true)
+val ShortArray.access get() = VolatileShortArray(this, true)
+val IntArray.access get() = VolatileIntArray(this, true)
+val LongArray.access get() = VolatileLongArray(this, true)
+val FloatArray.access get() = VolatileFloatArray(this, true)
+val DoubleArray.access get() = VolatileDoubleArray(this, true)
